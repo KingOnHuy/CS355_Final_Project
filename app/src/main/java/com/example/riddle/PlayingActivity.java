c9df8641b7558ff650dba6ef5ecccc986994fbf1
KingOnHuy <kingponpawit@gmail.com>
2020-11-17 23:45:42 +0700

    Update set manifest all activity no History

--- app/src/main/java/com/example/riddle/PlayingActivity.java
+++ app/src/main/java/com/example/riddle/PlayingActivity.java
@@ -2,7 +2,10 @@ package com.example.riddle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlayingActivity extends AppCompatActivity {
 
@@ -11,5 +14,22 @@ public class PlayingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("Let's play");
         setContentView(R.layout.activity_playing);
+
+        final Button giveUpBtn = (Button) findViewById(R.id.giveUpBtn);
+        final Button subminBtn = (Button) findViewById(R.id.subminBtn);
+
+        giveUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PlayingActivity.this, MainActivity.class));
+            }
+        });
+
+        subminBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PlayingActivity.this, ResultActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/riddle/ResultActivity.java
+++ app/src/main/java/com/example/riddle/ResultActivity.java
@@ -2,7 +2,10 @@ package com.example.riddle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        final Button replayBtn = (Button) findViewById(R.id.replayBtn);
+        final Button histotyBtn = (Button) findViewById(R.id.resultHistoryBtn);
+        final Button menuBtn = (Button) findViewById(R.id.menuBtn);
+
+        replayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultActivity.this, PlayingActivity.class));
+            }
+        });
+
+        histotyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultActivity.this, HistoryActivity.class));
+            }
+        });
+
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultActivity.this, MainActivity.class));
+            }
+        });
     }
 }
