b1e28b80faced5f4ebaa38b7f10a150594eea991
KingOnHuy <kingponpawit@gmail.com>
2020-11-17 21:31:33 +0700

    first commit

---
+++ app/src/main/java/com/example/riddle/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.riddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button startBtn = (Button) findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CategoryActivity.class));
+            }
+        });
+
+        final Button historyBtn = (Button) findViewById(R.id.historyBtn);
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+            }
+        });
+    }
+}
