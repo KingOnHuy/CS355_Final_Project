2d1de757a46dfae4bf5b672fa6fd065e634b47c8
KingOnHuy <kingponpawit@gmail.com>
2020-11-18 00:57:37 +0700

    Add time out, Try fix timer not stop when change activity.

--- app/src/main/java/com/example/riddle/PlayingActivity.java
+++ app/src/main/java/com/example/riddle/PlayingActivity.java
@@ -4,19 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PlayingActivity extends AppCompatActivity {
-
+    private int score = 0;
+    private CountDownTimer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Let's play");
         setContentView(R.layout.activity_playing);
 
+        final TextView timerTxt = (TextView) findViewById(R.id.timerTxt);
+        timer = new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timerTxt.setText(millisUntilFinished / 1000 + " Seconds remaining");
+            }
+
+            public void onFinish() {
+                timerTxt.setText("Timeout!");
+                startActivity(new Intent(PlayingActivity.this, ResultActivity.class));
+            }
+        }.start();
+
+
+        final TextView scoreTxt = (TextView) findViewById(R.id.scoreText);
         final Button giveUpBtn = (Button) findViewById(R.id.giveUpBtn);
         final Button subminBtn = (Button) findViewById(R.id.subminBtn);
+        final EditText wordInput = (EditText) findViewById(R.id.wordInput);
+
+        wordInput.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    subminBtn.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
 
         giveUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +61,15 @@ public class PlayingActivity extends AppCompatActivity {
         subminBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(PlayingActivity.this, ResultActivity.class));
+                score++;
+                scoreTxt.setText("Score: " + score);
             }
         });
+    }
+
+    @Override
+    public boolean onNavigateUp() {
+        timer.cancel();
+        return super.onNavigateUp();
     }
 }
